c35c07a64726905f859be1dd7745b97fb0f0497e
836hardik-agrawal <hardik.agrawal.cse21@itbhu.ac.in>
2025-07-08 23:53:40 +0530

    removed unnecessary code

--- flight_checker.py
+++ flight_checker.py
@@ -12,10 +12,7 @@ def get_flight_price(origin, destination, departure_date):
     
     # if not return_date:
     resp = requests.get(f"https://api.flightapi.io/onewaytrip/{API_KEY}/{origin}/{destination}/{departure_date}/1/0/0/Economy/INR")
-    # resp = requests.get('https://api.flightapi.io/onewaytrip/686d1ebf4e30db35ef995e50/BLR/DEL/2025-10-20/1/0/0/Economy/INR')
-    # print (resp.json())
     
-    # resp = requests.get('https://api.flightapi.io/onewaytrip/686d1ebf4e30db35ef995e50/BLR/DEL/2025-10-20/1/0/0/Economy/INR')
     if resp.status_code != 200:
         print("API Error:", resp.text)
         return None
@@ -35,32 +32,4 @@ def get_flight_price(origin, destination, departure_date):
        
     except (KeyError, IndexError):
         return None
-    
-# if __name__ == "__main__":
-
-#     # resp = requests.get('https://api.flightapi.io/onewaytrip/686d1ebf4e30db35ef995e50/BLR/DEL/2025-10-20/1/0/0/Economy/INR')
-#     # print (resp.json())
-    
-#     resp = requests.get(f"https://api.flightapi.io/onewaytrip/{API_KEY}/BLR/DEL/2025-10-20/1/0/0/Economy/INR")
-#     if resp.status_code != 200:
-#         print("API Error:", resp.text)
-#         # return None
-
-#     data = resp.json()
-#     # print(data)
-
-#     try:
-#         length = len(data["itineraries"])
-#         price = float('inf')
-#         for i in data["itineraries"]:
-#             if(price > i["cheapest_price"]["amount"]):
-#                 price = i["cheapest_price"]["amount"]
-
-#         # price = data["itineraries"][0]["cheapest_price"]["amount"]
-#         print(price)
-#     except (KeyError, IndexError):
-#         print(None)
-#         # return None
-#     # get_flight_price()
-
-
+ 
